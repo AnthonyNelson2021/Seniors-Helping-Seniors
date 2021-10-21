ed6ed19b6a65b70bf803f471d169d7d80050542b
sam <sammamer@gmail.com>
2021-10-21 15:46:31 -0400

    Changing title of screen and centering

--- app/src/main/java/com/example/seniorshelpingseniors/SettingsScreen.java
+++ app/src/main/java/com/example/seniorshelpingseniors/SettingsScreen.java
@@ -1,14 +1,26 @@
 package com.example.seniorshelpingseniors;
 
+import static com.example.seniorshelpingseniors.R.layout.abs_layout;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsScreen extends AppCompatActivity {
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+        // Changing title of screen to "Settings" + Centering these settings are found in abs_layout.xml file
+        androidx.appcompat.app.ActionBar customActionBar = getSupportActionBar();
+        customActionBar.setDisplayShowCustomEnabled(true);
+        customActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        customActionBar.setCustomView(abs_layout);
+
     }
 }
