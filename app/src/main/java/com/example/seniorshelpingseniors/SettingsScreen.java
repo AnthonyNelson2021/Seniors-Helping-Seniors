36a0419d0b46fade22ce4f4a96c72fbe90f0858a
sam <sammamer@gmail.com>
2021-11-01 17:24:43 -0400

    Fixed title bar and added support functionality

--- app/src/main/java/com/example/seniorshelpingseniors/SettingsScreen.java
+++ app/src/main/java/com/example/seniorshelpingseniors/SettingsScreen.java
@@ -5,10 +5,19 @@ import static com.example.seniorshelpingseniors.R.layout.abs_layout;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -23,23 +32,64 @@ import java.io.Console;
 import java.io.Writer;
 
 public class SettingsScreen extends AppCompatActivity {
-
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
 
-        // Changing title of screen to "Settings" + Centering these settings are found in abs_layout.xml file
-        androidx.appcompat.app.ActionBar customActionBar = getSupportActionBar();
-        customActionBar.setDisplayShowCustomEnabled(true);
-        customActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        customActionBar.setCustomView(abs_layout);
-
-        customActionBar.setHomeButtonEnabled(true);
+    }
+    public void onClickNotifications(View v) {
+        switch (v.getId()) {
+            // ...
+            case R.id.enable_tots_button:
+                enableNotifications();
+                break;
+            // ...
+        }
     }
 
-    public void onClick(View v) {
+    public void onClickSupport(View v) {
+        switch (v.getId()) {
+            // ...
+            case R.id.support_button:
+                showSupport();
+                break;
+            // ...
+        }
+    }
+
+    private void showSupport()
+    {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("US: 192-467-9090\nUK: 290-098-0022\nIN: 900-324-6789\nMX: 555-234-9124");
+        dialog.setTitle("Support Contacts");
+        dialog.setPositiveButton("Finished",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        //Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
+                    }
+        });
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
+    }
+
+    public void enableNotifications()
+    {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, String.valueOf(1))
+                .setSmallIcon(1)
+                .setContentTitle("Test notification")
+                .setContentText("This is a test")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
+    }
+
+    public void onClickLogout(View v) {
         switch (v.getId()) {
             // ...
             case R.id.logout_button:
