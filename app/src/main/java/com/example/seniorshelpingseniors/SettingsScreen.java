6d394075eef949d2e9389baa6a2b6a3cdfe112db
sam <sammamer@gmail.com>
2021-11-03 15:11:39 -0400

    Added functionality to reset button to prompt, sign user out, clear cache, and restart application.

--- app/src/main/java/com/example/seniorshelpingseniors/SettingsScreen.java
+++ app/src/main/java/com/example/seniorshelpingseniors/SettingsScreen.java
@@ -1,5 +1,6 @@
 package com.example.seniorshelpingseniors;
 
+import static android.content.ContentValues.TAG;
 import static com.example.seniorshelpingseniors.R.layout.abs_layout;
 
 
@@ -12,9 +13,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -29,6 +32,7 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.Console;
+import java.io.File;
 import java.io.Writer;
 
 public class SettingsScreen extends AppCompatActivity {
@@ -123,5 +127,86 @@ public class SettingsScreen extends AppCompatActivity {
     }
     // [END signOut]
 
+    public void onClickResetApp(View v) {
+        switch (v.getId()) {
+            // ...
+            case R.id.reset_button:
+                resetApplication();
+                break;
+            // ...
+        }
+    }
 
+    private void resetApplication()
+    {
+        AlertDialog.Builder builder
+                = new AlertDialog
+                .Builder(SettingsScreen.this);
+
+        // Set the message show for the Alert time
+        builder.setMessage("Are you sure you want to continue?\nYOU WILL BE SIGNED OUT\nAPP DATA WILL BE CLEARED");
+
+        // Set Alert Title
+        builder.setTitle("Alert!");
+
+        // Set Cancelable false
+        // for when the user clicks on the outside
+        // the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name
+        // OnClickListener method is use of
+        // DialogInterface interface.
+
+        builder
+                .setPositiveButton(
+                        "Yes",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+
+                                // When the user click yes button
+                                // then app will close
+                                signOut(); // Signing user out
+                                getCacheDir().delete(); // Clear cache
+                                finish(); // Close app
+                                startActivity(getIntent()); // Restart app
+
+                                // this basically provides animation
+                                overridePendingTransition(0, 0);
+                                String time = System.currentTimeMillis() + "";
+                                //finish();
+                            }
+                        });
+
+        // Set the Negative button with No name
+        // OnClickListener method is use
+        // of DialogInterface interface.
+        builder
+                .setNegativeButton(
+                        "No",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+
+                                // If user click no
+                                // then dialog box is canceled.
+                                dialog.cancel();
+                            }
+                        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+
+        // Show the Alert Dialog box
+        alertDialog.show();
+    }
 }
